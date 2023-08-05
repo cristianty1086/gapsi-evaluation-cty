ac5a35041210187ca7e1358156c54d0e17b3330a
cristianty1086 <$$gdEB32>
2023-08-05 09:24:16 -0500

    first commit

---
+++ gapsi-evaluation-cty/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  gapsi-evaluation-cty
+//
+//  Created by Cristian Tinoco Yurivilca on 5/08/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
